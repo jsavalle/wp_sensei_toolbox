28a1897b0694a19796c1b44b4d2d5ffd5a8b6596
JS <hello@jsavalle.com>
2019-02-03 13:06:46 +0100

    beta version - good for test in prod

--- index.php
+++ index.php
@@ -34,8 +34,8 @@ function sense_toolbox_reg_css_and_js($hook)
     wp_enqueue_style('boot_css', plugins_url('inc/bootstrap.css',__FILE__ ));
     wp_enqueue_style('boot_theme_css', plugins_url('inc/bootstrap_theme.css',__FILE__ ));
     wp_enqueue_style('toolbox_css', plugins_url('inc/toolbox.css',__FILE__ ));
-    wp_enqueue_script('boot_js', plugins_url('inc/bootstrap.js',__FILE__ ));
-    //wp_enqueue_script('ln_script', plugins_url('inc/main_script.js', __FILE__), ['jquery'], false, true);
+    wp_enqueue_script('boot_js', plugins_url('inc/bootstrap.js',__FILE__ ), ['jquery'], false, true);
+    wp_enqueue_script('toolbox_script', plugins_url('inc/toolbox.js', __FILE__), ['jquery'], false, true);
 }
 
 
@@ -57,15 +57,18 @@ function sensei_toolbox_admin_page() {
 
   // Start building the page
 
-  echo '<div class="wrap">';
+  echo '<div class="wrap sensei_toolbox">';
 
   echo '<h2>Sensei Toolbox</h2>';
+  echo '<h3>Export emails from students</h3>';
 
+
+  // Dropwdown for all courses
   $all_courses = WooThemes_Sensei_Course::get_all_courses();
   
-  $course_list   = "<label for='course_select'>Choose a course</label>";
-  $course_list  .= "<select name='course_select'>";
-  $course_list  .= "<option value='0'>-----------</option>";
+  $course_list   = "<label for='course_id_selected'>Select a course : &nbsp;</label>";
+  $course_list  .= "<select name='course_id_selected'>";
+  $course_list  .= "<option value='0'>----------------------</option>";
 
   if (count($all_courses) > 0){
     foreach( $all_courses as $course ){
@@ -82,14 +85,8 @@ function sensei_toolbox_admin_page() {
   if (isset($_POST['export_confirmed']) && check_admin_referer('export_confirmed_clicked')) {
     // the button has been pressed AND we've passed the security check
     $course_id_selected = $_POST['course_id_selected'];
-    if (is_numeric($course_id_selected) && $course_id_selected > 0){
-      export_emails_button_action($course_id_selected);
-    }
-    else {
-      echo '<div id="message" class="error fade"><p>'
-    .'No valid course id was provided .' . '</p></div>';
-
-    }
+    $status_selected = $_POST['status_selected'];
+    export_emails_button_action($course_id_selected, $status_selected);
   }
 
   echo '<form action="admin.php?page=sensei_toolbox-slug" method="post">';
@@ -97,21 +94,26 @@ function sensei_toolbox_admin_page() {
   // this is a WordPress security feature - see: https://codex.wordpress.org/WordPress_Nonces
   wp_nonce_field('export_confirmed_clicked');
   echo $course_list;
-  echo '<input type="text" name="course_id_selected" />';
+  echo '<div> 
+          <label for="status_selected">For status : &nbsp;</label><br>
+          <input type="radio" name="status_selected" value="complete"> Completed<br>
+          <input type="radio" name="status_selected" value="in-progress" checked> In Progress<br>
+        </div>';
+
   echo '<input type="hidden" value="true" name="export_confirmed" />';
-  submit_button('Call Function', 'primary', 'export_button', true);
+  submit_button('Export', 'primary', 'export_button', true);
   echo '</form>';
 
   echo '</div>';
 
 }
 
-function export_emails_button_action($course_id_selected)
+function export_emails_button_action($course_id_selected, $status_selected)
 {
-  echo '<div id="message" class="updated fade"><p>'
-    .'The "Call Function" button was clicked.' . '</p></div>';
+  echo '<div id="message" class="success fade"><p>'
+  .'No course was created yet .' . '</p></div></div>';
 
-  print("<pre> ## ".$course_id_selected." ##</pre>");
+  //print("<pre> ## ".$course_id_selected." ##</pre>");
 
     
   // Course ID can be found in the URL when you edit the course
@@ -128,9 +130,9 @@ function export_emails_button_action($course_id_selected)
   // run WP_Comment_Query to get the activity on the course
   $user_statusses = WooThemes_Sensei_Utils::sensei_check_for_activity( $activity_args, true );
 
-  print("<pre>");
-  print_r($user_statusses);
-  print("</pre>");
+  // print("<pre>");
+  // print_r($user_statusses);
+  // print("</pre>");
 
 
 //Now get all the users
@@ -139,15 +141,15 @@ foreach( $user_statusses as $activity ){
 	$users[] = get_user_by( 'id', $activity->user_id );
 }
 
-print("<pre>\n****************\n");
-print_r($users);
-print("</pre>");
+// print("<pre>\n****************\n");
+// print_r($users);
+// print("</pre>");
 
+print("<pre>");
   foreach( $users as $student ){
-    print("<pre>");
-    print_r($student->data);
-    print("</pre>");
+    print($student->data->user_email).", \n";
   }  
+print("</pre>");
 }
 
 ?>
